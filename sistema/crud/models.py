63a014e6f510a5eaef0c912b5470381e81df107b
Luis Velasquez <luis.velasquez.a@outlook.cl>
2022-06-14 03:11:27 -0400

    Conectando a base de datos Mysql por phpmyadmin

--- sistema/crud/models.py
+++ sistema/crud/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Curso(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100,verbose_name='Titulo')   
+    imagen = models.ImageField(upload_to='imagenes',verbose_name="imagen",null=True)
+    descripcion = models.TextField(verbose_name="Descripcion",null=True)
