cf6120e811020f8aa40ca8933c52d87622c338e7
Luis Velasquez <luis.velasquez.a@outlook.cl>
2022-06-15 02:44:31 -0400

    Mostrando los cursos creados en nuestro index

--- sistema/crud/models.py
+++ sistema/crud/models.py
@@ -8,4 +8,11 @@ class Curso(models.Model):
     imagen = models.ImageField(upload_to='imagenes',verbose_name="imagen",null=True)
     descripcion = models.TextField(verbose_name="Descripcion",null=True)
 
+def __str__(self):
+    fila = "Titulo: " + self.titulo + " - " + "Descripcion: " + self.descripcion
+    return fila
 
+
+def delete(self,using=None, keep_parents=False):
+    self.imagen.storage.delete(self.imagen.name)
+    super().delete()
--- sistema/crud/views.py
+++ sistema/crud/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Curso
 # Create your views here.
 
 def inicio(request):
@@ -10,7 +11,8 @@ def nosotros(request):
     return render(request,'nosotros.html')
 
 def cursos(request):
-    return render(request,'cursos/index.html')
+    cursos = Curso.objects.all()
+    return render(request,'cursos/index.html',{'cursos': cursos})
 
 def crear(request):
     return render(request,'cursos/crear.html')
