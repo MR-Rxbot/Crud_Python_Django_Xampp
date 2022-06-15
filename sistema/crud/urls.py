41f5a9f40024c236392e2510dc9dd01ce1950d99
Luis Velasquez <luis.velasquez.a@outlook.cl>
2022-06-15 03:14:07 -0400

    configurando seccion cursos para que se muestre contenido de DB

--- sistema/crud/urls.py
+++ sistema/crud/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
 
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+
+
 urlpatterns = [
     path('',views.inicio,name='inicio'),
     path('nosotros',views.nosotros, name='nosotros'),
     path('cursos',views.cursos,name='cursos'),
     path('crear',views.crear,name='crear'),
     path('editar',views.editar,name='editar'),
-]
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
